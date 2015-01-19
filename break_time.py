833cb85c29662e2a7a86db669144f3c52b4039e2
Dennis <denniszelada@gmail.com>
2015-01-18 18:21:57 -0600

    added cycle to repeat the code three times"

--- break_time.py
+++ break_time.py
@@ -1,5 +1,10 @@
 import webbrowser #import the webbrowser module
 import time #import the time module
 
-time.sleep(10)
-webbrowser.open("https://codility.com") #this open the browser on a specific page
+total_breaks = 3
+break_count = 1
+print ('This program started on '+time.ctime())
+while (break_count < total_breaks):
+    time.sleep(10)
+    webbrowser.open("https://codility.com") #this open the browser on a specific page
+    break_count = break_count + 1
