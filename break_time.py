7fc2c36b8c635504a6c8814077331361953a0003
Dennis <denniszelada@gmail.com>
2015-01-18 18:12:49 -0600

    added time module and a delay to open the website

--- break_time.py
+++ break_time.py
@@ -1,3 +1,5 @@
 import webbrowser #import the webbrowser module
+import time #import the time module
 
+time.sleep(10)
 webbrowser.open("https://codility.com") #this open the browser on a specific page
