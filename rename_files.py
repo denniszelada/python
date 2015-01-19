83860848965f9cbf809013ee9e9c4b7685fb5f39
Dennis <denniszelada@gmail.com>
2015-01-18 18:45:24 -0600

    added function to rename files

--- rename_files.py
+++ rename_files.py
@@ -5,5 +5,6 @@ def rename_files():
     file_list = os.listdir(r"/Users/dennis/projects/python/prank")
     print(file_list)
     #2. for each file, rename file
-
+    for file_name in file_list:
+        os.rename(file_name, file_name.translate(none, "0123456789"))
 rename_files()
